3d8be31fd57c2ee7b00381c9765c8bcaad4193ba
null <dwsong07@naver.com>
2021-06-28 00:17:51 +0900

    Add help command and remove test command

---
+++ src/commands/others/help.command.ts
@@ -0,0 +1,27 @@
+import commandType from "../type";
+import { prefix } from "../../../config.json";
+
+const command: commandType = {
+    name: "help",
+    description: "도움말",
+    usage: "",
+    execute(msg, args) {
+        const data = [];
+        const { commands } = msg.client;
+
+        data.push("```");
+        data.push("도움말");
+
+        commands.forEach((command) => {
+            data.push(`${prefix} ${command.name}`);
+            data.push(`\t${command.description}`);
+            data.push(`\t사용법: ${prefix} ${command.name} ${command.usage}`);
+        });
+
+        data.push("```");
+
+        msg.channel.send(data, { split: true });
+    },
+};
+
+export default command;
